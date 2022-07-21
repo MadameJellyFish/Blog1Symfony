8882298d6c0b22527621175e8e919973a0b57645
Ximena Gonzalez <xigonzalez28@gmail.com>
2022-07-21 10:28:22 +0200

    add filtre category

---
+++ src/Controller/Admin/CategoryCrudController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Category;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
+
+class CategoryCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Category::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+        ->setSearchFields(['post.name', 'post.id']);
+    }
+    
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+        ->add('name')
+        ->add('id');
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        // return [
+        //     IdField::new('id'),
+        //     TextField::new('title'),
+        //     TextEditorField::new('description'),
+        // ];
+        yield AssociationField::new('post');
+        yield TextField::new('name');
+        yield IdField::new('id');
+
+    }
+
+}
--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Category;
+use App\Entity\Post;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,7 +18,11 @@ class DashboardController extends AbstractDashboardController
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
+        // return parent::index();
+        $routeBuilder = $this->container->get(AdminUrlGenerator::class);
+        $url = $routeBuilder->setController(CategoryCrudController::class)->generateUrl();
+
+        return $this->redirect($url);
 
         // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
         // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
@@ -30,5 +38,9 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+       yield MenuItem::linkToRoute('Back to the website', 'fas fa-list', 'category.index');
+       yield MenuItem::linkToCrud('Category', 'fas fa-list', Category::class);
+    //    yield MenuItem::linkToCrud('Post', 'fas fa-list', Post::class);
+    //    yield MenuItem::linkToCrud('User', 'fas fa-list', User::class);
     }
 }
